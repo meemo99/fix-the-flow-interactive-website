3745d646f6a7a753520ecbe01ebc4c2e95b0708f
meemo99 <oumaima.el.fakir@hva.nl>
2025-12-05 09:43:02 +0100

    interactie js

---
+++ script.js
@@ -0,0 +1,6 @@
+//Selecteer de header
+let titleChange = document.querySelector('header');
+//Wacht tot iemand scrollt
+            window.addEventListener('scroll', function (){
+                //Verander de size, verklein de h1
+                document.querySelector('h1').classList.add('change')});
