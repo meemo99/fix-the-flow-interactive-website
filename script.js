964db8aed707464977c810c9aa1055086e7d7e79
meemo99 <oumaima.el.fakir@hva.nl>
2025-12-18 12:44:05 +0100

    Header scroll + dropdown filter + buttons toegevoegd
    #3

--- script.js
+++ script.js
@@ -6,11 +6,14 @@ let textChange = document.querySelector('h1');
                 //Verander de size, verklein de h1
                 if (this.window.scrollY > 0) {
                     document.querySelector('h1').classList.add('change')
+                    // Vergroot h1 bij terugscrollen
                 }else {
                     document.querySelector('h1').classList.remove('change')
                 }
+                // Verklein logo
                 if (this.window.scrollY > 0) {
                     document.querySelector('svg').classList.add('small')
+                    // vergroot logo bij terugscrollen
                 }else {
                     document.querySelector('svg').classList.remove('small')
                 }
@@ -22,8 +25,11 @@ let textChange = document.querySelector('h1');
 
 
 //Filter
+    // Selecteer introduction
 let filterDrop = document.querySelector('.introduction');
+    // Wacht tot iemand klikt
             filterDrop.addEventListener('click', function(){
+                // Toggle de filter dropdown
                 document.querySelector('.dropdown-content').classList.toggle('show');
             })
             
